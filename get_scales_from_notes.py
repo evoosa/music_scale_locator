4322d0ad27d942f64bbea04a979e53e84d8f5e6c
evoosa <eva@mypart.com>
2021-07-03 00:15:35 +0300

    now having pretty pink gui not working tho - added button

--- get_scales_from_notes.py
+++ get_scales_from_notes.py
@@ -1,15 +1,9 @@
-# get the notes
-# get the db
-# set the input, list it and repr
-# get the scales from the DB
-
-# from tkinter import Tk, Listbox, YES, END, NO
 from tkinter import *
-from utils import get_random_unicode_music_char
+
 from config import NOTES, OCTAVE
+from utils import get_random_unicode_music_char
 
 # Colors
-GREY = '#676767'
 LIGHT_GRAPEFRUIT = '#FFCFC4'
 LIGHT_GRAPEFRUIT_2 = '#FFABAB'
 LIGHT_GRAPEFRUIT_3 = '#FF7474'
@@ -17,35 +11,37 @@ BROWN = '#560F00'
 FONT_TUPLE = ("Berlin Sans FB", 20)
 
 window = Tk()
-window.geometry('800x800')
+window.geometry('800x500')
+window.configure(bg=LIGHT_GRAPEFRUIT)
 
-# Choosing selectmode as multiple
-# for selecting multiple options
-notes = Listbox(window,
-                selectmode="multiple",
-                bg=LIGHT_GRAPEFRUIT,
-                font=FONT_TUPLE,
-                fg=BROWN,
-                width=850, height=180,
-                highlightcolor=LIGHT_GRAPEFRUIT_3,
-                selectbackground=LIGHT_GRAPEFRUIT_3)
+notes_lb = Listbox(window,
+                   selectmode="multiple",
+                   bg=LIGHT_GRAPEFRUIT,
+                   font=FONT_TUPLE,
+                   fg=BROWN,
+                   width=850, height=10,
+                   highlightcolor=LIGHT_GRAPEFRUIT_3,
+                   selectbackground=LIGHT_GRAPEFRUIT_3)
 
-# Widget expands horizontally and
-# vertically by assigning both to
-# fill option
-notes.pack(expand=YES, fill=NONE, padx=5, pady=5)
-
-# Taking a list 'x' with the items
-# as languages
-x = ["C", "C++", "Java", "Python", "R",
-     "Go", "Ruby", "JavaScript", "Swift"]
+notes_lb.pack(expand=YES, fill=NONE, padx=100, pady=10)
 
 for idx in range(OCTAVE):
     line = f'{45 * " "}{get_random_unicode_music_char()}      {NOTES[idx]}{11 * " " if len(NOTES[idx]) == 1 else 3 * " "}   {get_random_unicode_music_char()}{45 * " "}'
-    notes.insert(END, line)
-    notes.itemconfig(idx,
-                     bg=LIGHT_GRAPEFRUIT if idx % 2 == 0 else LIGHT_GRAPEFRUIT_2,
+    notes_lb.insert(END, line)
+    notes_lb.itemconfig(idx, bg=LIGHT_GRAPEFRUIT if idx % 2 == 0 else LIGHT_GRAPEFRUIT_2)
 
-                     )
+
+def get_notes_selection():
+    return notes_lb.curselection()
+
+
+# Get Scales Button
+Button(window,
+       text='Get Scales',
+       bg=LIGHT_GRAPEFRUIT_2,
+       font=FONT_TUPLE,
+       fg=BROWN,
+       height=40,
+       command=get_notes_selection).pack(padx=50, ipady=7, ipadx=200, pady=20)
 
 window.mainloop()
