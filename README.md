db0dce21ebf3cacfedd510ce57a0434c5d5e3906
evoosa <eva@mypart.com>
2021-07-02 19:22:15 +0300

    added get_scales

--- README.md
+++ README.md
@@ -4,6 +4,3 @@ locates the corresponding scale to a given set of notes
 # reason for algorithm
 # why chose list / array
 
-# TODO
-# why i chose arrays (?), less memory? need to check memory usage of the finished product
-# add typing to functions
--- scales_sort_utils.py
+++ scales_sort_utils.py
@@ -1,3 +1,4 @@
+import pickle
 from typing import List
 
 from config import SCALES_TO_INTERVALS, NOTES
@@ -7,10 +8,16 @@ OCTAVE = 12
 
 def get_scales():
     """ Get all the scales """
-    # for i in range(OCTAVE):
-
-    pass
-
+    scales = {}
+    for note in range(OCTAVE):
+        note_name = NOTES[note]
+        for scale_type in SCALES_TO_INTERVALS:
+            scale_name = f'{note_name} {scale_type[0]}'
+            scale = get_scale(note, scale_type[1])
+            scales[scale_name] = scale
+            print(scale_name, get_scale_notes_names(scale))
+    print(f'Total of {len(scales)} scales')
+    return scales
 
 def get_scale(tonic: int, scale_intervals: list):
     """ Get the scale's notes given his tonic and it's intervals """
@@ -32,12 +39,20 @@ def get_sorted_scales():
     pass
 
 
-def pickle_sorted_scales():
+def pickle_data(data, output_file_path: str):
     """ Pickle the sorted data structure of the scales """
+    with open(output_file_path, 'wb') as handle:
+        pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
     pass
 
 
+def get_scale_notes_names(scale_notes: list):
+    """ Get the scale's notes names """
+    return [NOTES[i] for i in scale_notes]
+
+
 if __name__ == '__main__':
-    scale = get_scale(0, SCALES_TO_INTERVALS[2][1])
-    print(scale)
-    print([NOTES[i] for i in scale])
+    # scale = get_scale(0, SCALES_TO_INTERVALS[1][1])
+    # print(scale)
+    # print([NOTES[i] for i in scale])
+    get_scales()
